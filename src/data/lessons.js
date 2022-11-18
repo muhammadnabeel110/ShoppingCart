3de3668237a680c5bc7b47b72ae75a092e7c18b5
Nabeel <Nabeelshahbaz@gmail.com>
2022-11-18 21:01:50 +0530

    Add lessons to lessons.js

---
+++ src/data/lessons.js
@@ -0,0 +1,92 @@
+const lessons = [
+  {
+    id: 1,
+    subject: "Math",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-calculator fa-2x"
+  },
+  {
+    id: 2,
+    subject: "Math",
+    location: "Oxford",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-calculator fa-2x"
+  },
+  {
+    id: 3,
+    subject: "Biology",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-dna fa-2x"
+  },
+  {
+    id: 4,
+    subject: "English",
+    location: "York",
+    price: 90,
+    spaces: 5,
+    icon: "fa fa-e fa-2x"
+  },
+  {
+    id: 5,
+    subject: "Music",
+    location: "Bristol",
+    price: 90,
+    spaces: 5,
+    icon: "fa fa-music fa-2x"
+  },
+  {
+    id: 6,
+    subject: "Physics",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-wheelchair-move fa-2x"
+  },
+  {
+    id: 7,
+    subject: "Social Studies",
+    location: "Oxford",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-people fa-2x"
+  },
+  {
+    id: 8,
+    subject: "Social Studies",
+    location: "London",
+    price: 50,
+    spaces: 5,
+    icon: "fa fa-people fa-2x"
+  },
+  {
+    id: 9,
+    subject: "Science",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-flask fa-2x"
+  },
+  {
+    id: 10,
+    subject: "Science",
+    location: "Oxford",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-flask fa-2x"
+  },
+  {
+    id: 11,
+    subject: "English",
+    location: "London",
+    price: 100,
+    spaces: 5,
+    icon: "fa fa-e fa-2x"
+  }
+];
+
+export default lessons;
